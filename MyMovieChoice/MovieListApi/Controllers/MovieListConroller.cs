932993c38874f4d2b43a2a1db52fac5efec70f18
David Barbour <dbarbour99@yahoo.com>
2019-03-13 20:21:30 -0600

    Interim post

---
+++ MyMovieChoice/MovieListApi/Controllers/MovieListConroller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyMovieChoice.Data;
+using MyMovieChoice.Models;
+using System.Threading.Tasks;
+
+namespace MovieListApi.Controllers
+{
+  [Produces("application/json")]
+  [Route("api/MovieListConroller")]
+  public class MovieListConroller : Controller
+  {
+
+    private readonly MovieListContext _context;
+    public MovieListConroller(MovieListContext context)
+    {
+      _context = context;
+    }
+
+
+    public async Task<MovieList> GetMovie(int MovieListID)
+    {
+
+      var ThisMovie = await _context.MovieLists
+        .AsNoTracking()
+        .SingleOrDefaultAsync(m => m.MovieListID == MovieListID);
+
+      return ThisMovie;
+    }
+
+    public async Task<IActionResult> GetMovieList()
+    {
+      var movieContext = _context.MovieLists.Include(d => d.MovieListID);
+      return View(await movieContext.ToListAsync());
+    }
+
+  }
+}
+
